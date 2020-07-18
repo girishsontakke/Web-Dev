02eef1b26774f7ec836da9cc72476312ae12adcd
Girish Sontakke <girish@pop-os.localdomain>
2020-07-18 18:07:57 +0530

    object oriented programming in javascript

---
+++ javaScript/advance_object.js
@@ -0,0 +1,38 @@
+// ############ reference type ##########//
+let obj = {value:3};
+let obj1 = obj;
+let obj2 = {value:3};
+console.log(obj===obj1);
+console.log(obj===obj2);
+
+// ########### context vs scope ##########//
+const object = {
+    a : function(){
+        console.log(this);
+    }
+}
+object.a();
+
+// ############ instantiation ###########//
+class player{
+    constructor(name, type=newbie){
+        this.name=name;
+        this.type=type;
+    }
+    introduce(){
+        console.log(`Hi I am ${this.name}, I am a ${this.type}`);
+    }
+}
+
+class cricketer extends player{
+    constructor(name, type=newbie){
+        super(name, type);
+    }
+    play(){
+        console.log(`Hit a ball`);
+    }
+}
+const sachin = new cricketer('sachin', 'bastman');
+sachin.introduce();
+sachin.play();
+
