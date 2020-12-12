70389eb1997a4166d340e248c093ef920785fb5e
girishsontakke <girishsontakke7@gmail.com>
2020-12-12 10:40:47 +0530

    optional chaining operator

--- javaScript/ES2020.js
+++ javaScript/ES2020.js
@@ -11,16 +11,21 @@ console.log(typeof 1n);
 console.log(Number.MAX_SAFE_INTEGER);
 
 // Optional Chaining operator
+
+/* ?. operator functions similarly to the . chaining operator, except that instead of causing an error if a reference is nullish (null or undefined), the expression short-circuits with a return value of undefined
+ */
+
 let employ = {
   name: "girish",
   age: 20,
 };
 
-let salary = employ?.salary;
+let salary = employ.salary?.value;
 let age = employ?.age;
 
-console.log("salary" + salary);
-console.log("age" + age);
+console.log("salary = " + salary);
+console.log("age = " + age);
 
 // Nullish Coalescing Operator
-let efficiency = employ?.efficiency ?? "efficiency not given";
+let efficiency = employ.efficiency ?? "efficiency not given";
+console.log(efficiency);
